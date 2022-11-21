1c5b7f03da4656e36a7ce64093b8a6e5de5f36a4
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-11-21 23:54:54 +0100

    Starter Files Ready

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MainDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Source+Serif+Pro:wght@200;300;400;600;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          {/*Below we add the modal wrapper*/}
+          {/* <div id="modal-root"></div> */}
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MainDocument;
